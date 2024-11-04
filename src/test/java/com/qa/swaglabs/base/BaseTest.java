171420f87da8731fff9779b1f136f9eaa97faea5
Veena Hegde <veena444@gmail.com>
2024-11-04 15:39:32 +0530

    Added cross browser logic

--- src/test/java/com/qa/swaglabs/base/BaseTest.java
+++ src/test/java/com/qa/swaglabs/base/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
 import com.qa.swaglabs.factory.DriverFactory;
@@ -25,11 +26,17 @@ public class BaseTest {
 	protected ProductDetailsPage productDetailsPage;
 	protected CartPage cartPage;
 	
-	@BeforeTest
-	
-	public void setup() {
+	@Parameters({"browser"})
+	@BeforeTest	
+	public void setup(String browserName) {
 		df = new DriverFactory();
 		prop = df.initProp();
+		
+		//check if browser parameter is coming from testng.xml
+		if(browserName != null) {
+			prop.setProperty("browser", browserName);
+			}
+		
 		driver = df.initDriver(prop);
 		loginPage = new LoginPage(driver);
 		softAssert = new SoftAssert();
@@ -40,5 +47,8 @@ public class BaseTest {
 	public void tearDown() {
 		driver.quit();
 	}
+	
+
+
 
 }
